a15f28d11184349455323c5f302d87ea50d8cee9
Serena Li <serena.li.usa@gmail.com>
2021-08-14 23:56:02 -0700

    change example links

--- extension2/my-app/src/utils/apiCalls.ts
+++ extension2/my-app/src/utils/apiCalls.ts
@@ -3,12 +3,12 @@ import { Product } from "../types";
 export const getSuggestedProducts = async (keyword: string): Promise<Product[]> => {
   return [
     {
-      link: `http.com${Date.now()}`,
+      link: `https://www.walmart.com/laksdjf/`,
       name: "Name Of Productasdfaljsdf laksdjflkj as dlfsadfa sdfaskj lasjdkf lkajsd f",
       price: 100,
     },
     {
-      link: "https://www.bing.com",
+      link: "https://www.amazon.com/askdf/asdjlf/",
       name: "Name of Product 2 lasdjf alskdjfsl akdjflsaj kdfljkasdf ",
       price: 200,
     },
